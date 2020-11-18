5efbe60c2e7bddb4301fc2e58128be57f2cf6592
again7536 <hongxing107@gmail.com>
2020-11-18 18:17:25 +0900

    version 1.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './app.css';
 import boogleImage from './boogle.png';
+const google = require('google-search-results');
+
+function SearchItem(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      <h4>{props.url}</h4>
+      <p>{props.desc}</p>
+    </article>
+  );
+}
+
+function SearchContainer() {
+  const [keyword, setKeyword] = React.useState('');
+  const [result, setResult] = React.useState([]);
+  
+  function searchGoogle() {
+    google(keyword, (res)=> {
+      let items = [];
+      for(const r of res)
+        items.push(<SearchItem title={r.title} url={r.url} desc={r.description} ></SearchItem>);
+      setResult(items);
+    });
+  };
+
+  return (
+    <div className='search-form'>
+      <input id='search-bar' value={keyword} onChange={e => setKeyword(e.target.value)}/>
+      <button onClick={searchGoogle} id='search-btn'>Search!</button>
+      {result}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App"></div>
+    <>
+    <div className="App">
+      <header>
+        <img src={boogleImage} className='logo'></img>
+        <SearchContainer />
+      </header>
+    </div>
+    <div id="result">
+
+    </div>
+    </>
   );
-}
+};
 
 ReactDOM.render(
   <React.StrictMode>
